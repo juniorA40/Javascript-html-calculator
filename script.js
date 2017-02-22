a382939c04445628a411d13993f0d81d8bfbda40
junior <jalvarado@wlapcs.org>
2017-02-22 16:39:55 +0000

    new changes...

--- script.js
+++ script.js
@@ -8,6 +8,6 @@ function display(userInput){
 }
 
 function calculate(){
-    var answer = eval(document.getElementById("display-bar").innerHTML);
+    var answer = eval(document.getElementById("display-bar").value);
     document.getElementById("display-bar").value = answer;
 }
